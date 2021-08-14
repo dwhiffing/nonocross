d698955e9a5d3a64b1d7fb744b8c837c886c4371
Daniel Whiffing <danielwhiffing@gmail.com>
2021-08-14 16:04:50 -0400

    adds gridline

--- src/services/hud.js
+++ src/services/hud.js
@@ -11,6 +11,10 @@ export default class HudService {
   loadSolutionText = (rows, cols) => {
     this.rows = []
     this.cols = []
+    this.graphics = this.scene.add.graphics()
+    this.graphics.fillStyle(0x000000, 0.7)
+    this.graphics.fillRect(40, 0, 1, 90).setDepth(40)
+    this.graphics.fillRect(0, 40, 90, 1).setDepth(40)
     rows.forEach((item, i) =>
       this.rows.push(
         this.addText(3, 2 + i * 8, item.join(' ')).setScrollFactor(0, 1),
