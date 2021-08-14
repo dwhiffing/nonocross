7856a0aad347a637aec9675aee1ba00cb1e253ae
Daniel Whiffing <danielwhiffing@gmail.com>
2021-08-14 15:19:07 -0400

    Adds menu hotkey

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -10,15 +10,18 @@ export default class extends Phaser.Scene {
 
     const { width, height } = this.cameras.main
     this.background = new Background(this)
+    const start = () => {
+      this.scene.start('Game', { level: 1 })
+    }
+
+    this.input.keyboard.addKey('SPACE').addListener('down', start)
 
     this.add.image(5, 20, 'title', 56).setOrigin(0)
 
     this.add
       .image(width / 2 - 15, height - 10, 'tilemap', 56)
       .setInteractive()
-      .on('pointerdown', () => {
-        this.scene.start('Game', { level: 1 })
-      })
+      .on('pointerdown', start)
 
     this.add
       .image(width / 2 + 15, height - 10, 'tilemap', 57)
